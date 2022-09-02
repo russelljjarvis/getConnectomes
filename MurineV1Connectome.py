a87f983ccfed0408f26e6f05b8c9eea37c3d15eb
Russell Jarvis <rjarvis@westernsydney.edu.au>
2022-09-02 10:03:23 +1000

    merge togethor remote changes to README, and factor out dependencies

---
+++ MurineV1Connectome.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Download Allen V1 connectome
+# ### With minimal dependencies
+# 
+# SONATA, reads and writes hdf5 files.
+# 
+# ## Step 1
+# Download [this whole drop box directory](https://www.dropbox.com/sh/w5u31m3hq6u2x5m/AABGlA4w3k72vWmNndkyDbxZa/GLIF%20Network/network?dl=0&subfolder_nav_tracking=1). 
+# If you can find the wget syntax for this, I would be greatful for that.
+# Place the contents of that directory, in the same directory as this notebook.
+# ## Step 2
+# Pip install sonata and hdf5, as I am going to assume you don't already have those packages.
+# ## Step 3. 
+# * Arguably the data is already in it's ideal format for reading and writing graphs (big data/hdf5 no brainer).
+# 
+# * We are going to move the needle backwards, and transform the network into networkx, this will facilitate a transformation to Julia JLD files.
+
+import numpy as np
+import h5dict
+from sonata.circuit import File
+
+# ## Reading SONATA Network files
+
+
+netv1 = File(data_files=['v1_nodes.h5', 
+                       'v1_v1_edges.h5',
+                       'lgn_v1_edges.h5',
+                       'bkg_v1_edges.h5',
+                       'lgn_nodes.h5',
+                       'bkg_nodes.h5',
+                      ], 
+           data_type_files=['v1_node_types.csv',
+                            'v1_v1_edge_types.csv',
+                            'bkg_node_types.csv',
+                           ])
+
+
+
+v1_edges = netv1.edges
+
+import pandas as pd
+import tqdm
+import networkx as nx
+import matplotlib.pyplot as plt
+import pickle
+import pandas as pd
+
+print('Edge populations in file: {0}'.format(v1_edges.population_names))
+
+for key in v1_edges.population_names:
+    exec("g"+str(key)+str(" = nx.Graph()"))
+    exec("this_name="+str("g")+str(key))
+    
+    break
+print('Edge populations in file: {0}'.format(len(netv1.edges[key])))
+v1_v1_dict = h5dict.File(str("AllenV1Murinexx")+str(key)+str(".hdf5"), "w") 
+#filters = {"compression": "lzf"}
+
+this_key = netv1.edges[key]
+#with open("file_crash_index.p","wb") as f:
+#    index__ = pickle.load(f)            
+
+#this_key(str(edge.source_node_id),str(edge.target_node_id))
+with open("file_crash_index.p","wb") as f:
+    for index,edge in tqdm.tqdm(enumerate(this_key)):   
+        #if index__ > index:    
+        if True:
+            temp = {}  
+            if hasattr(edge,"plastic"):
+                temp["plastic"] = True
+            else:
+                temp["plastic"] = False 
+            if hasattr(edge,"delay"):
+                temp["delay"] = edge.delay
+            else:
+                temp["delay"] = False
+            if hasattr(edge,"nsyns"):
+                temp["nsyns"] = edge.nsyns
+            else:
+                temp["nsyns"] = 1.0
+            if hasattr(edge,"syn_weight"): 
+                this_name.add_edge(str(edge.source_node_id),str(edge.target_node_id),weight=edge.syn_weight)        
+            else:
+                this_name.add_edge(str(edge.source_node_id),str(edge.target_node_id),weight=0.0)
+            for k,v in temp.items():
+                this_name.edges[str(edge.source_node_id),str(edge.target_node_id)][k] = v 
+            pickle.dump(index,f)            
+ 
+v1_v1_dict["network"] = this_name
+v1_v1_dict.close()
+test_dict = h5dict.File(str("AllenV1Murine")+str(key)+str(".hdf5"), "r")    
+test_dict.tree_view(print_types=True)
+G = test_dict["network"]
+assert hasattr(G,"edges")
+v1_v1_dict.close()
+
+
+
