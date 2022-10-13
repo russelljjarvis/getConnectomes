23aed9b0e9bbf89bd4bbcc3890559e6011a72bed
Russell Jarvis <russelljarvis@protonmail.com>
2022-10-14 09:30:09 +1100

    Update and rename load_allen_net.jl to load_allen_measured_model.jl

--- load_allen_measured_model.jl
+++ load_allen_measured_model.jl
@@ -8,26 +8,33 @@ using SparseArrays
 #https://cloudstor.aarnet.edu.au/plus/s/fFOrhM2ZrIAClZa
 ##
 function get_full_arrays()
+    """
+    Int64 is used rather than Int32 because 70million is a big number and all values in between must be represented.
+    """
     h5f = h5open("v1_source_node_id.h5","r") 
-    srcs = read(h5f["source_node_id"])
-    h5f = h5open("v1_target_node_id.h5","r") 
-    tgts = read(h5f["target_node_id"])
+    srcs = Vector{Int64}(read(h5f["source_node_id"]))
     close(h5f)
-
+    h5f = h5open("v1_target_node_id.h5","r") 
+    tgts = Vector{Int64}(read(h5f["target_node_id"]))
+    close(h5f)
     return (srcs, tgts)    
 end
 
 function get_weights_delays(begin_,end_)
     h5f = h5open("v1_node_id_e.h5","r") 
     enodes = Vector{Int64}(read(h5f["node_id_e"]))
+    close(h5f)
     h5f = h5open("v1_node_id_i.h5","r") 
     inodes = Vector{Int64}(read(h5f["node_id_i"]))
+    close(h5f)
     h5f = h5open("v1_syn_weight.h5","r") 
-    syn_weights = read(h5f["syn_weight"])[begin_:end_]   
+    syn_weights = Vector{Int64}(read(h5f["syn_weight"])[begin_:end_])   
+    close(h5f)
     h5f = h5open("v1_delay.h5","r") 
-    delays = read(h5f["delay"])[begin_:end_]   
+    delays = Vector{Int64}(read(h5f["delay"])[begin_:end_])   
+    close(h5f)
     h5f = h5open("v1_nsyns.h5","r")
-    nsyns = read(h5f["nsyns"])[begin_:end_]
+    nsyns = Vector{Int64}(read(h5f["nsyns"])[begin_:end_])
     close(h5f)
 
     return (enodes,inodes,syn_weights,delays,nsyns)
